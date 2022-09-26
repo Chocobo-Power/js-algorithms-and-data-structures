e5b4be97c1d8678a6dd75442529cce4c3840e44e
Renzo Belon <renzobelon@gmail.com>
2022-09-26 12:45:51 -0500

    Added 35-05-multiple-pointers-average-pair/averagePair.js

--- 35-05-multiple-pointers-average-pair/averagePair.js
+++ 35-05-multiple-pointers-average-pair/averagePair.js
@@ -15,6 +15,32 @@
  * averagePair([],4) // false
  */
 
-function averagePair(){
+function averagePair(arr, target){
     // add whatever parameters you deem necessary - good luck!
+
+    if (arr.length < 2) {
+        return false;
+    }
+
+    let ptr1 = 0;
+    let ptr2 = arr.length-1;
+
+    while (ptr1 != ptr2) {
+        const average = (arr[ptr1] + arr[ptr2] / 2);
+        if (average === target) {
+            return true;
+        } else if (average > target) {
+            ptr2--;
+        } else {
+            ptr1++;
+        }
+    }
+    
+    return false;
 }
+
+// Sample Input:
+console.log(averagePair([1,2,3],2.5)); // true
+console.log(averagePair([1,3,3,5,6,7,10,12,19],8)); // true
+console.log(averagePair([-1,0,3,4,5,6], 4.1)); // false
+console.log(averagePair([],4)); // false
